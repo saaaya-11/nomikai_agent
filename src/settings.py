006caf23649ed8210d634a5ce2c45351827b14c9
saaaya-11 <sayaka-nakamoto@outlook.jp>
2025-06-03 00:17:50 +0900

    Update: Added google api key

--- src/settings.py
+++ src/settings.py
@@ -3,6 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv(os.path.join(os.path.dirname(__file__), '../.env'))
 
+# Gemini
+GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
+GOOGLE_ENDPOINT_OPENAI = "https://generativelanguage.googleapis.com/v1beta/openai/"
+
+# OpenAI
+OpenAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
 # Hotpepper (Ref: https://webservice.recruit.co.jp/doc/hotpepper/reference.html)
 RECRUIT_API_KEY = os.environ.get("RECRUIT_API_KEY")
-HOTPEPPER_ENDPOINT_SEARCH = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
